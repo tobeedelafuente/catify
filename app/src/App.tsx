3c670c9dd5c4df9882d355254095ada766b2542a
tobeedelafuente <tobeedelafuente@gmail.com>
2021-05-06 20:54:27 +0800

    Initial project commit

---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cat Browser</h1>
+      <label>Breed</label>
+      <select name="breed">
+        <option value="">Select breed</option>
+      </select>
+    </div>
+  );
+}
+
+export default App;
